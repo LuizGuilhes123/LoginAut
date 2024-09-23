2bdd33a270ca9b13fff175a66edc09895ba7e83e
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2024-09-23 19:07:35 -0300

    feat: foi adicionado os códigos de segurança e configuração do jwt e spring security #2

---
+++ src/main/java/com/loginAut/LoginAut/domain/User.java
@@ -0,0 +1,23 @@
+package com.loginAut.LoginAut.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String Id;
+    private String Name;
+    private String Password;
+    private String Email;
+}
