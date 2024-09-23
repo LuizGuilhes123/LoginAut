b50bb15f2cb587de9b47f241469f67194dbb2937
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2024-09-23 17:38:17 -0300

    feat: foi adicionado as depedency de jwt e postgreSQL #1

---
+++ src/main/java/com/loginAut/LoginAut/LoginAutApplication.java
@@ -0,0 +1,13 @@
+package com.loginAut.LoginAut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loginAut/LoginAut/LoginAutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginAut.LoginAut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
